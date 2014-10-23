1e8f33800f1dda190d3cdbd84c7b4324b5b5a8bf
Liam Wickins <liamw9534@gmail.com>
2014-10-23 18:06:39 +0100

    Made state changes async.

--- mopidy_alsa/sink.py
+++ mopidy_alsa/sink.py
@@ -10,10 +10,11 @@ import gst  # noqa
 class AlsaAudioSink(gst.Bin):
     def __init__(self, address):
         super(AlsaAudioSink, self).__init__()
+        queue = gst.element_factory_make('queue')
         alsa = gst.element_factory_make('alsasink')
         alsa.set_property('device', address)
         alsa.set_property('sync', False)
-        queue = gst.element_factory_make('queue')
+        alsa.set_property('async', True)
         self.add_many(queue, alsa)
         gst.element_link_many(queue, alsa)
         pad = queue.get_pad('sink')
